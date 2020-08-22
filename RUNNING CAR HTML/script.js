f25be67885ea9dfc813e401b0efb0187fa68f933
bhavi1402 <68499873+bhavi1402@users.noreply.github.com>
2020-08-22 19:51:55 +0530

    added new file

---
+++ RUNNING CAR HTML/script.js
@@ -0,0 +1,8 @@
+//var audio = document.createElement('audio');
+//audio.setAttribute('src', 'sound.mp3'); 
+//audio.loop=true;
+//audio.play();
+
+window.onload=function(){
+document.getElementById("tah_audio").play();
+}
